01269c10ef37e4f6abe515cb4fdb8962a54ceada
akbarakma <akbarajo1234@gmail.com>
2020-02-27 17:27:36 +0700

    add tv movies

--- controllers/index.js
+++ controllers/index.js
@@ -24,6 +24,47 @@ class Controller{
     static signup(req,res){
         res.render('register')
     }
+    static seeMovie(req, res) {
+        let movieId = req.params.movieId;
+        let movieUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=ee55e28c81ef2d96e16ed4fd23a15778&language=en-US`;
+        axios.get(movieUrl)
+            .then(movie => {
+                let movieData = movie.data;
+                res.send(movieData);
+            }).catch(err => {
+                res.send(err);
+            });
+    }
+    static seeTvSeries(req, res) {
+        let tvId = req.params.tvId;
+        let tvUrl = `https://api.themoviedb.org/3/tv/${tvId}?api_key=ee55e28c81ef2d96e16ed4fd23a15778&language=en-US`;
+        axios.get(tvUrl)
+            .then(tv => {
+                let tvData = tv.data;
+                res.send(tvData);
+            }).catch(err => {
+                res.send(err);
+            }); 
+    }
+    static tvHome(req, res) {
+        let popularTvUrl = `https://api.themoviedb.org/3/tv/popular?api_key=ee55e28c81ef2d96e16ed4fd23a15778&language=en-US&page=1`;
+        let onTheAirUrl = `https://api.themoviedb.org/3/tv/on_the_air?api_key=ee55e28c81ef2d96e16ed4fd23a15778&language=en-US&page=1`;
+        let topRatedTvUrl = `https://api.themoviedb.org/3/tv/top_rated?api_key=ee55e28c81ef2d96e16ed4fd23a15778&language=en-US&page=1`;
+        axios.all([
+            axios.get(popularTvUrl),
+            axios.get(onTheAirUrl),
+            axios.get(topRatedTvUrl)
+        ])
+            .then(axios.spread((popularTv, onTheAir, topRated) => {
+                let popularTvData = popularTv.data.results;
+                let onTheAirData = onTheAir.data.results;
+                let topRatedData = topRated.data.results;
+                res.send(topRatedData);
+            })).catch(err => {
+                res.send(err);
+            });
+
+    }
 }
 
 module.exports = Controller
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const Controller = require("../controllers");
 
 routes.get("/", Controller.home);
 routes.get("/register", Controller.signup);
-
+routes.get("/movies/:movieId", Controller.seeMovie);
+routes.get('/tv', Controller.tvHome);
+routes.get('/tv/:tvId', Controller.seeTvSeries);
 module.exports = routes;
