3eee2e555f5ed34d8283d43560b30148d275af53
darindra23 <58590046+darindra23@users.noreply.github.com>
2020-02-28 03:10:46 +0700

    lagi

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,20 @@ const routes = require("express").Router();
 const Controller = require("../controllers");
 
 const checkLogin = (req, res, next) => {
-    if (!req.session.user) {
-        console.log(req.session.user);
-        res.redirect('/login');
-    } else {
-        next();
-    }
-}
+  if (!req.session.user) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
 
 routes.get("/", Controller.home);
 routes.get("/register", Controller.register);
 routes.post("/register", Controller.registerData);
 routes.get("/login", Controller.login);
 routes.post("/login", Controller.loginData);
-routes.get('/movieslist', Controller.movieList);
-routes.get('/movies/:movieId', Controller.showMovie);
-routes.get('/user/:userId', checkLogin, Controller.user);
-routes.get('/movies/addwatchlist/:movieId/:userId', checkLogin, Controller.addWatchList);
+routes.get("/movieslist", Controller.movieList);
+routes.get("/user/:userId", checkLogin, Controller.user);
+routes.get("/movies/addwatchlist/:movieId/:userId",checkLogin,Controller.addWatchList);
 
 module.exports = routes;
