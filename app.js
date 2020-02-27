93e9d8a0737bcf23f1821c15c414ef4ddaef009d
darindra23 <58590046+darindra23@users.noreply.github.com>
2020-02-28 06:06:33 +0700

    tinggal deploy doang bang

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 3001;
-const routes = require('./routes');
-const session = require('express-session')
-
+const routes = require("./routes");
+const session = require("express-session");
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(session({
-    secret: 'darindra',
+app.use(
+  session({
+    secret: "darindra",
     resave: true,
     saveUninitialized: true,
     cookie: { secure: false }
-}));
-app.use(express.static('pictures'))
-app.use('/', routes)
+  })
+);
+app.use(express.static("pictures"));
+app.use("/", routes);
 
 app.listen(port, () => console.log("Listening On Port :", port));
--- models/user.js
+++ models/user.js
@@ -2,40 +2,75 @@
 module.exports = (sequelize, DataTypes) => {
   const Sequelize = sequelize.Sequelize;
   const Model = Sequelize.Model;
-  const { bycrpt } = require('../helpers/bycrypt');
+  const { bycrpt } = require("../helpers/bycrypt");
 
   class User extends Model {
     getFullName() {
-      return this.first_name + ' ' + this.last_name;
+      return this.first_name + " " + this.last_name;
     }
   }
-  User.init({
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    username: {
-      type: DataTypes.STRING,
-      validate: {
-        notEmpty: {
-          args: true,
-          msg: `Username cannot be empty`
+  User.init(
+    {
+      first_name: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            args: true,
+            msg: `First name cannot be empty`
+          }
+        }
+      },
+      last_name: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            args: true,
+            msg: `Last name cannot be empty`
+          }
+        }
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          notEmpty: {
+            args: true,
+            msg: `Username cannot be empty`
+          }
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          notEmpty: {
+            args: true,
+            msg: `E-Mail cannot be empty`
+          }
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [6, 20],
+            msg: `Password must be between 6 and 20 character`
+          }
         }
       }
     },
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    hooks: {
-      beforeCreate: (instance, options) => {
-        let password = instance.password;
-        return bycrpt(password)
-          .then(data => {
+    {
+      sequelize,
+      hooks: {
+        beforeCreate: (instance, options) => {
+          let password = instance.password;
+          return bycrpt(password).then(data => {
             instance.password = data;
-          })
+          });
+        }
       }
     }
-  })
-
+  );
 
   // const User = sequelize.define('User', {
   //   name: DataTypes.STRING,
